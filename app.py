9437d97edb7d168550f942af65f62898a3d5d60f
dissehkoffi44-eng <dissehkoffi44@gmail.com>
2025-12-28 13:18:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ TELEGRAM_TOKEN = st.secrets.get("TELEGRAM_TOKEN", "7751365982:AAFLbeRoPsDx5OyIOl
 CHAT_ID = st.secrets.get("CHAT_ID", "-1003602454394")
 
 # --- CONFIGURATION PAGE ---
-st.set_page_config(page_title="RCDJ228 MUSIC KEY PRO", page_icon="🎧", layout="wide")
+st.set_page_config(page_title="RCDJ228 MUSIC 3", page_icon="🎧", layout="wide")
 
 # --- STYLES CSS ---
 st.markdown("""
@@ -259,7 +259,7 @@ def get_full_analysis(file_bytes, file_name):
     return res
 
 # --- INTERFACE UTILISATEUR ---
-st.title("🎧 RCDJ228 MUSIC KEY PRO")
+st.title("🎧 RCDJ228 MUSIC 3")
 
 with st.sidebar:
     st.header("⚙️ SYSTÈME")
