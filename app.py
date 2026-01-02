e3722023b13cceb03a26589223b03579591ad9c2
dissehkoffi44-eng <dissehkoffi44@gmail.com>
2026-01-02 09:17:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ TELEGRAM_TOKEN = st.secrets.get("TELEGRAM_TOKEN", "7751365982:AAFLbeRoPsDx5OyIOl
 CHAT_ID = st.secrets.get("CHAT_ID", "-1003602454394")
 
 # --- CONFIGURATION PAGE ---
-st.set_page_config(page_title="RCDJ228 HARMONIC 3 PRO", page_icon="🎧", layout="wide")
+st.set_page_config(page_title="RCDJ228 Hkey 3 PRO", page_icon="🎧", layout="wide")
 
 # --- STYLES CSS ---
 st.markdown("""
@@ -255,7 +255,7 @@ def get_full_analysis(file_bytes, file_name):
     return res
 
 # --- INTERFACE ---
-st.title("🎧 RCDJ228 HARMONIC 3 PRO")
+st.title("🎧 RCDJ228 Hkey 3 PRO")
 
 with st.sidebar:
     st.header("⚙️ SYSTÈME")
