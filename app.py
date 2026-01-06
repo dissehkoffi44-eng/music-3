669b1aa09cfc4460413148a9bafa69b4366e653a
dissehkoffi44-eng <dissehkoffi44@gmail.com>
2026-01-06 11:58:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -63,14 +63,16 @@ st.markdown("""
     
     /* Box de décision finale */
     .final-decision-box { 
-        padding: 40px; 
-        border-radius: 25px; 
-        text-align: center; 
-        margin: 15px 0; 
-        border: 1px solid rgba(255,255,255,0.1); 
-        box-shadow: 0 10px 30px rgba(0,0,0,0.5);
-        background: linear-gradient(135deg, #1e3a8a 0%, #FFFFFF 100%);
-    }
+    padding: 40px; 
+    border-radius: 25px; 
+    text-align: center; 
+    margin: 15px 0; 
+    border: 1px solid rgba(255,255,255,0.1); 
+    box-shadow: 0 10px 30px rgba(0,0,0,0.5);
+    /* Modifiez la couleur ici si vous ne voulez pas qu'elle soit dynamique */
+    background: #2ecc71; 
+    color: white; /* Force le texte en blanc */
+}
     
     /* Tags des profils */
     .profile-tag { 
